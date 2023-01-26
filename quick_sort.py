b7ed5662d509575ca016e25e588b15491716393a
Ireanne-Omega <116430505+Ireanne-Omega@users.noreply.github.com>
2023-01-27 06:58:31 +0800

    final commit for quick sort

--- quick_sort.py
+++ quick_sort.py
@@ -6,9 +6,9 @@ print("BSCOE 2-2\n")
 # Q U I C K  S O R T
 def quick_sort(OwnArray, left, right):
     if left < right:
-        partition_pos = partition(OwnArray, left, right)
-        quick_sort(OwnArray, left, partition_pos - 1)
-        quick_sort(OwnArray, partition_pos + 1, right)
+        partitionpos = partition(OwnArray, left, right)
+        quick_sort(OwnArray, left, partitionpos - 1)
+        quick_sort(OwnArray, partitionpos + 1, right)
 
 
 def partition(OwnArray, left, right):
@@ -25,12 +25,12 @@ def partition(OwnArray, left, right):
 
         if i < j:
             OwnArray[i], OwnArray[j] = OwnArray[j], OwnArray[i]
-            print("\t\t", OwnArray)
+            print("\t", OwnArray)
 
     if OwnArray[i] > pivot:
         OwnArray[i], OwnArray[right] = OwnArray[right], OwnArray[i]
 
-        print("\t\t", OwnArray)
+        print("\t",OwnArray)
 
     return i
 
@@ -39,7 +39,7 @@ OwnArray = [53, 86, 25, 96, 79, 65, 37, 28, 80, 16]
 # P R I N T  S T A T E M E N T
 print("\n============================ QUICK SORT ==========================")
 print("\n\t\t\t\t\t\tUnsorted Array List:\n\t\t\t", OwnArray, "\n")
-print("\n\nSelection Sorting:")
+print("\nSelection Sorting:")
 quick_sort(OwnArray, 0, len(OwnArray) - 1)
 print("\n\n\t\t\t\t\tSorted Array List:\n\t\t\t", OwnArray, "\n")
 print("\n===================================================================")
